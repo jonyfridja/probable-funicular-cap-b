16471c785123304ad328c676ae4878c8af7e6e5c
Jonathan Fridja <johnfridja@gmail.com>
2021-08-13 13:35:28 +0300

    removed mock data

--- routes/transactions/transactions.mock.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    { id: '123', tradingparty: "me", counterparty: "adam", amount: -400 },
-    { id: '13', tradingparty: "adam", counterparty: "me", amount: 500 },
-    { id: '1', tradingparty: "me", counterparty: "someone_else", amount: 100 },
-]
