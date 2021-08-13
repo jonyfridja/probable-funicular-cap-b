57e2fb6b527a7b1894f07f441452335b4a04f1a6
Jonathan Fridja <johnfridja@gmail.com>
2021-08-13 10:42:31 +0300

    data fix

--- routes/transactions/transactions.mock.js
+++ routes/transactions/transactions.mock.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { id: '123', tradingParty: "me", counterparty: "you", amount: -400 },
-    { id: '13', tradingParty: "me", counterparty: "you", amount: 500 },
-    { id: '1', tradingParty: "me", counterparty: "someone_else", amount: 100 },
+    { id: '123', tradingparty: "me", counterparty: "you", amount: -400 },
+    { id: '13', tradingparty: "me", counterparty: "you", amount: 500 },
+    { id: '1', tradingparty: "me", counterparty: "someone_else", amount: 100 },
 ]
