58894ab2f6bddc90e419d433b2bb25e162a2c932
Jonathan Fridja <johnfridja@gmail.com>
2021-08-13 12:21:07 +0300

    better mocked data

--- routes/transactions/transactions.mock.js
+++ routes/transactions/transactions.mock.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { id: '123', tradingparty: "me", counterparty: "you", amount: -400 },
-    { id: '13', tradingparty: "me", counterparty: "you", amount: 500 },
+    { id: '123', tradingparty: "me", counterparty: "adam", amount: -400 },
+    { id: '13', tradingparty: "adam", counterparty: "me", amount: 500 },
     { id: '1', tradingparty: "me", counterparty: "someone_else", amount: 100 },
 ]
